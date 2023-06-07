391d85012094e027005c1dca5c0290ef17abc2f4
olliekemp <103338687+olliekemp@users.noreply.github.com>
2023-06-07 13:38:59 +0100

    Create setup.py

---
+++ py/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='your-module-name',
+    version='0.1',
+    description='Description of your module',
+    author='Your Name',
+    packages=[''],
+    install_requires=[
+        # Add any dependencies required by your module here
+    ],
+)
