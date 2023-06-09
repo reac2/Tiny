feb2999f7b5961f95356637ff02f41d0e2db16e8
olliekemp <103338687+olliekemp@users.noreply.github.com>
2023-06-09 09:30:45 +0100

    Update setup.py

--- py/setup.py
+++ py/setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
+with open("requirements.txt", 'r') as dependencies:
+    requirements = [pkg.strip() for pkg in dependencies]
+
 setup(
     name='tinymltoolkit',
     version='0.1',
     description='Functions to train and compress neural networks',
     author='Ollie Kemp',
-    install_requires=[
-        'numpy',
-        'tensorflow',
-        'optuna',
-        'scikit-learn',
-    ],
+    packages = setuptools.find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.10',
 )
