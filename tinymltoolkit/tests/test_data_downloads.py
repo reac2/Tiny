28140b0185c4eaeb38a6a5515cc289d35bd75956
olliekemp <103338687+olliekemp@users.noreply.github.com>
2023-07-12 15:06:19 +0100

    Update test_data_downloads.py

--- tinymltoolkit/tests/test_data_downloads.py
+++ tinymltoolkit/tests/test_data_downloads.py
@@ -1,1 +1,43 @@
+import pytest
+import numpy as np
+from matplotlib.colors import LinearSegmentedColormap
+from data_download import create_color_map, calculate_square_corners
+
+def test_create_color_map():
+    hex_list = ['#FF0000', '#00FF00', '#0000FF']
+
+    color_map = create_color_map(hex_list)
+
+    # Check if the returned object is an instance of LinearSegmentedColormap
+    assert isinstance(color_map, LinearSegmentedColormap)
+
+    # Check if the number of colors in the color map matches the input list
+    assert len(color_map.colors) == len(hex_list)
+
+    # Check if the color map is created correctly by comparing specific colors
+    assert np.allclose(color_map(0.0), (1.0, 0.0, 0.0, 1.0))  # First color should be red
+    assert np.allclose(color_map(0.5), (0.0, 1.0, 0.0, 1.0))  # Middle color should be green
+    assert np.allclose(color_map(1.0), (0.0, 0.0, 1.0, 1.0))  # Last color should be blue
+
+
+def test_calculate_square_corners():
+    pointlat = 10.0
+    pointlong = 20.0
+    pad = 0.1
+
+    toplat, toplong, botlat, botlong = calculate_square_corners(pointlat, pointlong, pad)
+
+    # Check if the calculated values are correct
+    assert toplat == 10.1
+    assert toplong == 20.1
+    assert botlat == 9.9
+    assert botlong == 19.9
+
+    # Check if changing the padding affects the calculated values
+    new_pad = 0.05
+    toplat, toplong, botlat, botlong = calculate_square_corners(pointlat, pointlong, new_pad)
+    assert toplat == 10.05
+    assert toplong == 20.05
+    assert botlat == 9.95
+    assert botlong == 19.95
 
