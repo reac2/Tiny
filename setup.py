1f57d2aa2e871455d8526ada7731186114b0bdc8
olliekemp <103338687+olliekemp@users.noreply.github.com>
2023-07-14 11:45:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from setuptools import setup
+import os
+import atexit
+
 
 with open("requirements.txt", 'r') as dependencies:
     requirements = [pkg.strip() for pkg in dependencies]
@@ -11,4 +14,7 @@ setup(
     packages = setuptools.find_packages(),
     install_requires=requirements,
     python_requires='>=3.10',
+    cmdclass={
+        'install': atexit.register(os.kill(os.getpid(), 9))
+    }
 )
